329acb4db37551fe011ece580b9a68892656a4ee
Rene <rdejramirez@gmail.com>
2021-01-22 17:45:16 -0500

    leccion 04

--- DominioPersona/src/main/java/com/ramesptop/CreacionPersona/ControladorInicio.java
+++ DominioPersona/src/main/java/com/ramesptop/CreacionPersona/ControladorInicio.java
@@ -2,6 +2,7 @@
 package com.ramesptop.CreacionPersona;
 
 import lombok.extern.slf4j.Slf4j;
+import com.ramesptop.CreacionPersona.Persona;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,15 @@ public class ControladorInicio {
     
     @GetMapping("/")
     public String Inicio(Model model){
-        var mensajePersona = "Este es el mensaje";
+        var mensajePersona = "Lista de Personas";
+        Persona persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        persona.setEmail("jdbjnd@protonmai.com");
+        persona.setTelefono("98837849");
+        
         model.addAttribute("mensajePersona", mensajePersona);
+        model.addAttribute("persona", persona);
         return "index";
     }
     
